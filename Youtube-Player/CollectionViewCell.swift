c2b48a3d5a3803bb50d9fe3ab93a096e052fec5a
Raj Abishek <rajabishek@hotmail.com>
2016-08-10 18:35:32 +0530

    Removes all the storyboard references completely

---
+++ Youtube-Player/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Youtube-Player
+//
+//  Created by Raj Abishek on 10/08/16.
+//  Copyright © 2016 Raj Abishek. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
